f63fe14a1421aabb5aeda1c10531a28fc29cb2d2
Earl <devaahmad43@gmail.com>
2025-04-14 20:37:43 +0700

    dart library

---
+++ example/hello.dart
@@ -0,0 +1,13 @@
+import 'package:belajar_dart_package_library/hello.dart';
+
+void main(){
+  print(sayHello("Napoleon Bonaparte"));
+
+  var customer = Customer();
+  var product = Product();
+  var category = Category();
+
+  print(customer);
+  print(product);
+  print(category);
+}
